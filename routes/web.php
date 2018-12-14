50ed82b8476134874c9d1bd0753ec606b8b62b8c
vAlfred88 <valfred88@gmail.com>
2018-12-14 09:52:11 +0300

    Add black index page when login company admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::domain('my.' . config('app.url'))->group(function () {
+    Route::get('/', function () {
+        return view('index');
+    });
     Auth::routes();
 });
 
