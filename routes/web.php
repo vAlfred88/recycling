9112abccadab116ebe4bee7a5dd597ab92687e91
vAlfred88 <valfred88@gmail.com>
2018-12-16 11:52:27 +0300

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::domain('my.' . config('app.url'))->group(function () {
     Route::middleware('auth')->group(function () {
+        Route::get('/', function () {
+            return view('index');
+        })->name('company.home');
         Route::resource('companies', 'CompanyController');
         Route::get('profile', 'ProfileController@view')
             ->name('profile.view');
         Route::put('profile', 'ProfileController@update')
             ->name('profile.update');
     });
-    Route::get('/', function () {
-        return view('index');
-    })->name('company.home');
 
     Auth::routes();
 });
