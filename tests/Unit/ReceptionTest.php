49975b0ed3eb7bae378398ba7ce0315e9677c552
vAlfred88 <valfred88@gmail.com>
2018-12-21 18:01:01 +0300

    Add testing to receptions

---
+++ tests/Unit/ReceptionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReceptionTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $reception;
+
+    /** @test */
+    public function reception_belongs_to_a_company()
+    {
+        $this->assertInstanceOf('App\Company', $this->reception->company);
+    }
+
+    /** @test */
+    public function reception_can_belongs_to_user()
+    {
+        $user = create('App\User');
+
+        $this->reception->users()->attach($user);
+
+        $this->assertTrue($this->reception->users->contains($user));
+    }
+
+    /** @test */
+    public function reception_has_reviews()
+    {
+        $review = create('App\Review');
+
+        $this->reception->reviews()->attach($review);
+
+        $this->assertTrue($this->reception->reviews->contains($review));
+    }
+
+    /** @test */
+    public function reception_has_a_service()
+    {
+        $service = create('App\Service');
+
+        $this->reception->services()->attach($service);
+
+        $this->assertTrue($this->reception->services->contains($service));
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->reception = create('App\Reception');
+    }
+}
