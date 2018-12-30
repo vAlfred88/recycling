aa7805232d309fab07bbbb9ccfc13dfa19a0ebe9
vAlfred88 <valfred88@gmail.com>
2018-12-30 14:18:23 +0300

    New company card

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.master')
 
 @push('css')
+    <style>
+        .row-eq-height {
+            display: -webkit-box;
+            display: -webkit-flex;
+            display: -ms-flexbox;
+            display: flex;
+        }
+    </style>
 @endpush
 
 @section('content')
@@ -37,7 +45,7 @@
                         <div class="panel panel-default">
                             <div class="panel-body">
                                 <div class="text-center">
-                                    <img src="{{asset('images\logo.png')}}" alt="" class="img-fluid" width="100%">
+                                    <img src="{{asset('images\metal.png')}}" alt="" class="img-fluid" width="100%">
                                 </div>
                                 <div class="panel-footer text-center">
                                     <h3>{{ optional($company)->name }}</h3>
@@ -60,6 +68,41 @@
                         </div>
                     </div>
                     <div class="col-lg-8">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <div class="col-lg-8">
+                                    Статус публикации
+                                </div>
+                                <div class="col-lg-4 pull-right">
+                                    <button class="btn btn-sm btn-block btn-outline btn-default">
+                                        Открыть рейтинг
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <h5>Вы находитесь на 12м месте в рейтинге ломозаготовителей</h5>
+                                <div class="progress progress-lg">
+                                    <div class="progress-bar progress-bar-warning active progress-bar-striped"
+                                         style="width: 75%;"
+                                         role="progressbar">
+                                        <span class="text-dark">Анкета заполнена на 75%</span>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-4 text-left">
+                                        <a href="#">Снять компанию с публикации</a>
+                                    </div>
+                                    <div class="col-lg-8 text-right">
+                                        <i class="icon-bulb"></i>
+                                        <span>Внимание, компания не будет доступна в поиске и в рейтинге</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
                         <div class="panel panel-default">
                             <div class="panel-heading ">
                                 <div class="row">
@@ -97,9 +140,9 @@
                                     <h5>{{ optional($company)->address }}</h5>
                                 </div>
                                 <div class="col-lg-4">
-                                    <h5>ИНН 7834545455</h5>
-                                    <h5>КПП 4352345345</h5>
-                                    <h5>ОГРН 345346555235</h5>
+                                    <h5>ИНН {{ optional($company)->inn }}</h5>
+                                    <h5>КПП {{ optional($company)->kpp }}</h5>
+                                    <h5>ОГРН {{ optional($company)->ogrn }}</h5>
                                 </div>
                             </div>
                         </div>
@@ -107,75 +150,43 @@
                 </div>
             </div>
         </div>
+        {{--<div class="row">--}}
+        {{--<div class="col-lg-12">--}}
+        {{--<div class="panel panel-default">--}}
+        {{--<div class="panel-heading">--}}
+        {{--<div class="col-lg-5">--}}
+        {{--Услуги доступные в пунктах приема--}}
+        {{--</div>--}}
+        {{--<div class="col-lg-">--}}
+        {{--<div class="col-lg-4 pull-right">--}}
+        {{--<button class="btn btn-sm btn-block btn-outline btn-default">--}}
+        {{--Открыть пункты приема--}}
+        {{--</button>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="panel-body">--}}
+        {{--<div class="row">--}}
+        {{--<div class="col-lg-6">--}}
+        {{--<p>Лом цветных металлов</p>--}}
+        {{--<p>Черный металлолом</p>--}}
+        {{--<p>Демонтаж металлоконструкций</p>--}}
+        {{--<p>Вывоз металлолома</p>--}}
+        {{--</div>--}}
+        {{--<div class="col-lg-6">--}}
+        {{--<p>Лом цветных металлов</p>--}}
+        {{--<p>Черный металлолом</p>--}}
+        {{--<p>Демонтаж металлоконструкций</p>--}}
+        {{--<p>Вывоз металлолома</p>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
         <div class="row">
             <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="col-lg-5">
-                            Услуги доступные в пунктах приема
-                        </div>
-                        <div class="col-lg-">
-                            <div class="col-lg-4 pull-right">
-                                <button class="btn btn-sm btn-block btn-outline btn-default">
-                                    Открыть пункты приема
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <p>Лом цветных металлов</p>
-                                <p>Черный металлолом</p>
-                                <p>Демонтаж металлоконструкций</p>
-                                <p>Вывоз металлолома</p>
-                            </div>
-                            <div class="col-lg-6">
-                                <p>Лом цветных металлов</p>
-                                <p>Черный металлолом</p>
-                                <p>Демонтаж металлоконструкций</p>
-                                <p>Вывоз металлолома</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="col-lg-4">
-                            Статус публикации
-                        </div>
-                        <div class="col-lg-8">
-                            <div class="col-lg-4 pull-right">
-                                <button class="btn btn-sm btn-block btn-outline btn-default">
-                                    Открыть рейтинг
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-                        <h5>Вы находитесь на 12м месте в рейтинге ломозаготовителей</h5>
-                        <div class="progress progress-lg">
-                            <div class="progress-bar progress-bar-warning active progress-bar-striped"
-                                 style="width: 75%;"
-                                 role="progressbar">
-                                <span class="text-dark">Анкета заполнена на 75%</span>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-4 text-left">
-                                <a href="#">Снять компанию с публикации</a>
-                            </div>
-                            <div class="col-lg-8 text-right">
-                                <i class="icon-bulb"></i>
-                                <span>Внимание, компания не будет доступна в поиске и в рейтинге</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
