60d31c93c2fef04070d5d550b7dbe8e28e9d6a0d
vAlfred88 <valfred88@gmail.com>
2018-12-30 12:49:24 +0300

    Change logo

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -5,37 +5,35 @@
                 <div class="dropdown user-pro-body ">
                     <div class="profile-image">
                         <img src="{{asset('plugins/images/users/jeffery.jpg')}}" alt="user-img" class="img-circle">
-                        <a href="javascript:void(0);" class="dropdown-toggle u-dropdown text-blue"
-                           data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            <span class="badge badge-danger">
-                                <i class="fa fa-angle-down"></i>
-                            </span>
-                        </a>
-                        <ul class="dropdown-menu animated flipInY">
-                            <li>
-                                <a href="{{route('profile.view')}}">
-                                    <i class="fa fa-user"></i>Профиль
-                                </a>
-                            </li>
-                            <li role="separator" class="divider"></li>
-                            <li>
-                                <a class="dropdown-item"
-                                   href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                        <p class="profile-text m-t-15 font-16">
+                            <a href="javascript:void(0);">{{ $user->name }}</a>
+                        </p>
+                        <nav class="sidebar-nav">
+                            <ul id="side-menu">
+                                <li class="two-column">
+                                    <a href="{{route('profile.view')}}">
+                                        <i class="fa fa-user"></i>
+                                        <span class="hide-menu">Профиль</span>
+                                    </a>
+                                </li>
+                                <li class="two-column">
+                                    <a class="dropdown-item"
+                                       href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    <i class="fa fa-power-off"></i>Выйти
-                                    <form id="logout-form"
-                                          action="{{ route('logout') }}"
-                                          method="POST"
-                                          style="display: none;">
-                                        @csrf
-                                    </form>
-                                </a></li>
-                        </ul>
+                                        <i class="fa fa-power-off"></i>
+                                        <span class="hide-menu">Выйти</span>
+                                        <form id="logout-form"
+                                              action="{{ route('logout') }}"
+                                              method="POST"
+                                              style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
-                    <p class="profile-text m-t-15 font-16">
-                        <a href="javascript:void(0);">{{ $user->name }}</a>
-                    </p>
                 </div>
             </div>
         @endif
