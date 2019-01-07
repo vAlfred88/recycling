55b0a3ab7d325f2ef57e874cfde195460c746799
vAlfred88 <valfred88@gmail.com>
2019-01-07 16:05:57 +0400

    Fix companies blade template

---
+++ resources/views/companies/card.blade.php
@@ -0,0 +1,189 @@
+@extends('layouts.master')
+
+@push('css')
+
+@endpush
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="text-left col-lg-4 text-left">
+                                Моя компания
+                            </div>
+                            <div class="text-left col-lg-8 pull-right">
+                                <div class="row">
+                                    <div class="col-lg-8 text-right">
+                                        <span class="text-muted text-lowercase"
+                                              style="font-size: 1.25rem">
+                                            зарегистрирована с 21 декаюря 2018
+                                        </span>
+                                    </div>
+                                    <div class="col-lg-4 pull-right">
+                                        <a href="{{ route('companies.edit', $company) }}"
+                                           class="btn btn-sm btn-block btn-outline btn-warning">
+                                            Редактировать
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="text-center">
+                                    <img src="{{asset('images\metal.png')}}" alt="" class="img-fluid" width="100%">
+                                </div>
+                                <div class="panel-footer text-center">
+                                    <h3>{{ optional($company)->name }}</h3>
+                                    <div class="row">
+                                        <button class="btn btn-facebook waves-effect btn-circle waves-light"
+                                                type="button">
+                                            <i class="fa fa-facebook"></i>
+                                        </button>
+                                        <button class="btn btn-twitter waves-effect btn-circle waves-light"
+                                                type="button">
+                                            <i class="fa fa-twitter"></i>
+                                        </button>
+                                        <button class="btn btn-googleplus waves-effect btn-circle waves-light"
+                                                type="button">
+                                            <i class="fa fa-google-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <div class="col-lg-8">
+                                    Статус публикации
+                                </div>
+                                <div class="col-lg-4 pull-right">
+                                    <button class="btn btn-sm btn-block btn-outline btn-default">
+                                        Открыть рейтинг
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <h5>Вы находитесь на 12м месте в рейтинге ломозаготовителей</h5>
+                                <div class="progress progress-lg">
+                                    <div class="progress-bar progress-bar-warning active progress-bar-striped"
+                                         style="width: 75%;"
+                                         role="progressbar">
+                                        <span class="text-dark">Анкета заполнена на 75%</span>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-4 text-left">
+                                        <a href="#">Снять компанию с публикации</a>
+                                    </div>
+                                    <div class="col-lg-8 text-right">
+                                        <i class="icon-bulb"></i>
+                                        <span>Внимание, компания не будет доступна в поиске и в рейтинге</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading ">
+                                <div class="row">
+                                    <div class="text-left col-lg-4 text-left">
+                                        О компании
+                                    </div>
+                                    <div class="text-left col-lg-8 text-right">
+                                        <div class="col-lg-5 pull-right">
+                                            <button class="btn btn-sm btn-block btn-outline btn-default">
+                                                Открыть на сайте
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                {{ optional($company)->description }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Реквизиты компании
+                            </div>
+                            <div class="panel-body text-left">
+                                <div class="col-lg-4">
+                                    <h5><a href="#" class="text-warning">{{ optional($company)->site }}</a></h5>
+                                    <h5>{{ optional($company)->phone }}</h5>
+                                    <h5>{{ optional($company)->email }}</h5>
+                                </div>
+                                <div class="col-lg-4">
+                                    <h5>{{ optional($company)->address }}</h5>
+                                </div>
+                                <div class="col-lg-4">
+                                    <h5>ИНН {{ optional($company)->inn }}</h5>
+                                    <h5>КПП {{ optional($company)->kpp }}</h5>
+                                    <h5>ОГРН {{ optional($company)->ogrn }}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{--<div class="row">--}}
+        {{--<div class="col-lg-12">--}}
+        {{--<div class="panel panel-default">--}}
+        {{--<div class="panel-heading">--}}
+        {{--<div class="col-lg-5">--}}
+        {{--Услуги доступные в пунктах приема--}}
+        {{--</div>--}}
+        {{--<div class="col-lg-">--}}
+        {{--<div class="col-lg-4 pull-right">--}}
+        {{--<button class="btn btn-sm btn-block btn-outline btn-default">--}}
+        {{--Открыть пункты приема--}}
+        {{--</button>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="panel-body">--}}
+        {{--<div class="row">--}}
+        {{--<div class="col-lg-6">--}}
+        {{--<p>Лом цветных металлов</p>--}}
+        {{--<p>Черный металлолом</p>--}}
+        {{--<p>Демонтаж металлоконструкций</p>--}}
+        {{--<p>Вывоз металлолома</p>--}}
+        {{--</div>--}}
+        {{--<div class="col-lg-6">--}}
+        {{--<p>Лом цветных металлов</p>--}}
+        {{--<p>Черный металлолом</p>--}}
+        {{--<p>Демонтаж металлоконструкций</p>--}}
+        {{--<p>Вывоз металлолома</p>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        <div class="row">
+            <div class="col-lg-12">
+
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('js')
+@endpush
--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,192 +1,89 @@
 @extends('layouts.master')
 
 @push('css')
-    <style>
-        .row-eq-height {
-            display: -webkit-box;
-            display: -webkit-flex;
-            display: -ms-flexbox;
-            display: flex;
-        }
-    </style>
+    <link href="{{asset('plugins/components/datatables/jquery.dataTables.min.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.dataTables.min.css" rel="stylesheet"
+          type="text/css"/>
 @endpush
 
 @section('content')
     <div class="container-fluid">
+        <!-- .row -->
         <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="text-left col-lg-4 text-left">
-                                Моя компания
-                            </div>
-                            <div class="text-left col-lg-8 pull-right">
-                                <div class="row">
-                                    <div class="col-lg-8 text-right">
-                                        <span class="text-muted text-lowercase"
-                                              style="font-size: 1.25rem">
-                                            зарегистрирована с 21 декаюря 2018
-                                        </span>
-                                    </div>
-                                    <div class="col-lg-4 pull-right">
-                                        <a href="{{ route('companies.edit', $company) }}"
-                                           class="btn btn-sm btn-block btn-outline btn-warning">
-                                            Редактировать
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <div class="text-center">
-                                    <img src="{{asset('images\metal.png')}}" alt="" class="img-fluid" width="100%">
-                                </div>
-                                <div class="panel-footer text-center">
-                                    <h3>{{ optional($company)->name }}</h3>
-                                    <div class="row">
-                                        <button class="btn btn-facebook waves-effect btn-circle waves-light"
-                                                type="button">
-                                            <i class="fa fa-facebook"></i>
-                                        </button>
-                                        <button class="btn btn-twitter waves-effect btn-circle waves-light"
-                                                type="button">
-                                            <i class="fa fa-twitter"></i>
-                                        </button>
-                                        <button class="btn btn-googleplus waves-effect btn-circle waves-light"
-                                                type="button">
-                                            <i class="fa fa-google-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <div class="col-lg-8">
-                                    Статус публикации
-                                </div>
-                                <div class="col-lg-4 pull-right">
-                                    <button class="btn btn-sm btn-block btn-outline btn-default">
-                                        Открыть рейтинг
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="panel-body">
-                                <h5>Вы находитесь на 12м месте в рейтинге ломозаготовителей</h5>
-                                <div class="progress progress-lg">
-                                    <div class="progress-bar progress-bar-warning active progress-bar-striped"
-                                         style="width: 75%;"
-                                         role="progressbar">
-                                        <span class="text-dark">Анкета заполнена на 75%</span>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4 text-left">
-                                        <a href="#">Снять компанию с публикации</a>
-                                    </div>
-                                    <div class="col-lg-8 text-right">
-                                        <i class="icon-bulb"></i>
-                                        <span>Внимание, компания не будет доступна в поиске и в рейтинге</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="panel panel-default">
-                            <div class="panel-heading ">
-                                <div class="row">
-                                    <div class="text-left col-lg-4 text-left">
-                                        О компании
-                                    </div>
-                                    <div class="text-left col-lg-8 text-right">
-                                        <div class="col-lg-5 pull-right">
-                                            <button class="btn btn-sm btn-block btn-outline btn-default">
-                                                Открыть на сайте
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel-body">
-                                {{ optional($company)->description }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                Реквизиты компании
-                            </div>
-                            <div class="panel-body text-left">
-                                <div class="col-lg-4">
-                                    <h5><a href="#" class="text-warning">{{ optional($company)->site }}</a></h5>
-                                    <h5>{{ optional($company)->phone }}</h5>
-                                    <h5>{{ optional($company)->email }}</h5>
-                                </div>
-                                <div class="col-lg-4">
-                                    <h5>{{ optional($company)->address }}</h5>
-                                </div>
-                                <div class="col-lg-4">
-                                    <h5>ИНН {{ optional($company)->inn }}</h5>
-                                    <h5>КПП {{ optional($company)->kpp }}</h5>
-                                    <h5>ОГРН {{ optional($company)->ogrn }}</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {{--<div class="row">--}}
-        {{--<div class="col-lg-12">--}}
-        {{--<div class="panel panel-default">--}}
-        {{--<div class="panel-heading">--}}
-        {{--<div class="col-lg-5">--}}
-        {{--Услуги доступные в пунктах приема--}}
-        {{--</div>--}}
-        {{--<div class="col-lg-">--}}
-        {{--<div class="col-lg-4 pull-right">--}}
-        {{--<button class="btn btn-sm btn-block btn-outline btn-default">--}}
-        {{--Открыть пункты приема--}}
-        {{--</button>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="panel-body">--}}
-        {{--<div class="row">--}}
-        {{--<div class="col-lg-6">--}}
-        {{--<p>Лом цветных металлов</p>--}}
-        {{--<p>Черный металлолом</p>--}}
-        {{--<p>Демонтаж металлоконструкций</p>--}}
-        {{--<p>Вывоз металлолома</p>--}}
-        {{--</div>--}}
-        {{--<div class="col-lg-6">--}}
-        {{--<p>Лом цветных металлов</p>--}}
-        {{--<p>Черный металлолом</p>--}}
-        {{--<p>Демонтаж металлоконструкций</p>--}}
-        {{--<p>Вывоз металлолома</p>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        <div class="row">
-            <div class="col-lg-12">
+            <div class="col-sm-12">
+                <div class="white-box">
+                    <h3 class="box-title pull-left">{{ __('permissions.permission') }}</h3>
+                    @can('add-'.str_slug('Companies'))
+                        <a class="btn btn-success pull-right" href="{{ route('permissions.create') }}">
+                            <i class="icon-plus"></i> {{ __('permissions.create') }}
+                        </a>
+                    @endcan
+                    <div class="clearfix"></div>
+                    <hr>
+                    <div class="table-responsive">
+                        <table class="table" id="myTable">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>{{ __('fields.name') }}</th>
+                                <th>{{ __('fields.label') }}</th>
+                                <th>{{ __('fields.actions') }}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($companies as $company)
+                                <tr>
+                                    <td>{{ $loop->iteration or $company->id }}</td>
+                                    <td>
+                                        @can('view-'.str_slug('Permissions'))
+                                            <a href="{{ route('companies.show', $company) }}"
+                                               title="{{ __('fields.more') }}">
+                                                {{ $company->name }}
+                                            </a>
+                                        @else
+                                            {{ $company->name }}
+                                        @endcan
+                                    </td>
+                                    <td>
+                                        @can('view-'.str_slug('Companies'))
+                                            @if($company->owner)
+                                                <a href="{{ route('users.show', $company->owner) }}"
+                                                   title="{{ __('fields.more') }}">
+                                                    {{ optional($company->owner)->name }}
+                                                </a>
+                                            @endif
+                                        @else
+                                            {{ optional($company->owner)->name }}
+                                        @endcan
+                                    </td>
+                                    <td>
+                                        @can('edit-'.str_slug('Permissions'))
+                                            <a href="{{ route('permissions.edit', $company) }}"
+                                               title="{{ __('fields.update') }}">
+                                                <button class="btn btn-primary btn-sm">
+                                                    <i class="fa fa-pencil-square-o"
+                                                       aria-hidden="true"></i> {{ __('fields.update') }}
+                                                </button>
+                                            </a>
+                                        @endcan
 
+                                        @can('delete-'.str_slug('Permissions'))
+                                            {!! Form::open(['route' => ['permissions.destroy', $company], 'style' => 'display:inline', 'method' => 'DELETE']) !!}
+
+                                            {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ' . __('fields.delete'), ['class' => 'btn btn-danger btn-sm', 'title' => __('fields.delete'), 'onclick'=>'return confirm("Confirm delete?")', 'type' => 'submit', 'id' => 'delete']) !!}
+
+                                            {!! Form::close() !!}
+                                        @endcan
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="pagination-wrapper">
+                            {!! $companies->appends(['search' => Request::get('search')])->render() !!}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
