bcf0c8ed3a86034f4c6a2e7454563d41fc7003b7
vAlfred88 <valfred88@gmail.com>
2019-01-07 14:46:26 +0400

    Add new blade template for admin panel

---
+++ resources/views/companies/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="white-box">
+                    <h3 class="box-title pull-left">{{ __('company.create') }}</h3>
+                    @can('create', \App\Company::class)
+                        <a class="btn btn-success pull-right" href="{{ route('companies.index') }}">
+                            <i class="icon-arrow-left-circle" aria-hidden="true"></i> {{ __('pages.back') }}</a>
+                    @endcan
+                    <div class="clearfix"></div>
+                    <hr>
+
+                    @if ($errors->any())
+                        <ul class="alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+
+                    {!! Form::open(['route' => 'companies.create', 'method' => 'POST', 'class' => 'form-horizontal']) !!}
+
+                    @include ('companies.form', ['submitButtonText' => __('fields.create')])
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
