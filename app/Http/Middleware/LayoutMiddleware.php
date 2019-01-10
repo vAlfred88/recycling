079507494c977f7ebab59ad15c4c70d0729e0524
vAlfred88 <valfred88@gmail.com>
2019-01-10 14:54:31 +0300

    Update composer, cleanup theme layout

--- app/Http/Middleware/LayoutMiddleware.php
+++ app/Http/Middleware/LayoutMiddleware.php
@@ -12,29 +12,29 @@ class LayoutMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $theme = ['normal','fix-header','mini-sidebar'];
-        if(isset(request()->theme)){
-            if($request->isMethod('get') && in_array($request->theme,$theme)){
-                Session::put('theme-layout',str_slug(request()->theme,'-'));
-            }else{
-                //dd($request->path());
+        $theme = ['normal', 'fix-header', 'mini-sidebar'];
+        if (isset(request()->theme)) {
+            if ($request->isMethod('get') && in_array($request->theme, $theme)) {
+                Session::put('theme-layout', str_slug(request()->theme, '-'));
+            } else {
                 $query = $request->query();
-                Session::put('theme-layout','normal');
+                Session::put('theme-layout', 'normal');
             }
-            return back();
 
-        }else{
-            if(!session()->has('theme-layout')){
-                Session::put('theme-layout','normal');
+            return back();
+        } else {
+            if (!session()->has('theme-layout')) {
+                Session::put('theme-layout', 'normal');
             }
         }
-        //session()->save();
+
         return $next($request);
     }
 }
