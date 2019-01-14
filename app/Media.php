48bce68cce277cb07d603582632f7d1673aac0f9
vAlfred88 <valfred88@gmail.com>
2019-01-14 12:11:20 +0300

    Add phpdoc block

--- app/Media.php
+++ app/Media.php
@@ -4,15 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Media
+ *
+ * @package App
+ */
 class Media extends Model
 {
+    /**
+     * @var string
+     */
     protected $table = 'files';
 
+    /**
+     * @var array
+     */
     protected $fillable = [
         'path',
         'name',
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
     public function storable()
     {
         return $this->morphTo();
