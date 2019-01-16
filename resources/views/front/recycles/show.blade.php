701adeb6b1abe84c430aa32788620247be2874c9
vAlfred88 <valfred88@gmail.com>
2019-01-16 14:44:01 +0300

    Fix errors

--- resources/views/front/recycles/show.blade.php
+++ resources/views/front/recycles/show.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="flex items-center">
-        <div class="md:w-full xl:w-3/4 md:mx-auto px-10">
+        <div class="md:w-full xl:w-3/4 md:mx-auto pt-10">
             <div class="mt-5 align-baseline flex">
                 <div class="flex-1 text-left text-orange-light inline">5 место в рейтинге</div>
                 <div class="flex-1 text-right">Отзывы
@@ -14,9 +14,9 @@
                     <span class="text-red">-3</span>
                 </div>
             </div>
-            <div class="shadow-lg">
+            <div class="shadow-lg bg-white">
                 <div class="flex mt-1 py-10">
-                    <div class="w-1/3">
+                    <div class="w-1/3 pl-5">
                         <div class="overflow-hidden">
                             <img class="w-full px-10 pt-5" src="{{ asset('images/metal.png') }}"
                                  alt="{{ $company->name }}">
@@ -28,11 +28,11 @@
                             </div>
                         </div>
                     </div>
-                    <div class="w-1/3 pt-5">
+                    <div class="w-1/3 py-5">
                         <div class="text-base mb-2 text-center">О компании</div>
                         <p class="text-grey-darker text-base">{{ $company->description }}</p>
                     </div>
-                    <div class="w-1/3 pt-5">
+                    <div class="w-1/3 pr-5">
                         <div class="text-base mb-2 text-center">Реквизиты компании</div>
                         <p class="text-orange-light text-base pt-2">{{ $company->site }}</p>
                         <p class="text-grey-darker text-base pt-2">{{ $company->phone }}</p>
@@ -54,18 +54,20 @@
         </div>
     </div>
     <div class="flex items-center">
-        <div class="md:w-full xl:w-3/4 md:mx-auto px-10">
-            <div class="mt-5 align-baseline flex">
+        <div class="md:w-full xl:w-3/4 md:mx-auto pt-10">
+            <div class="mt-5">
                 <div class="shadow-lg w-full">
-                    <div class="w-1/3">
+                    <div class="flex-wrap flex m-1 px-10 pt-5 align-baseline">
                         @foreach($users as $user)
-                            <div class="py-10">
+                            <div class="py-10 w-1/4 flex-col">
                                 <div class="w-1/2 mx-auto">
                                     <img class="rounded-full" src="{{ $user->image }}" alt="{{ $user->name }}">
                                 </div>
-                                <div class="text-base text-center">{{ $user->name }}</div>
-                                <p class="text-orange-light text-center">{{ $user->position }}</p>
-                                <p class="text-grey-dark text-center">{{ $user->email }}</p>
+                                <div class="px-3">
+                                    <p class="text-base text-center">{{ $user->name }}</p>
+                                    <p class="text-orange-light text-center py-2">{{ $user->position }}</p>
+                                    <p class="text-grey-dark text-center break-words">{{ $user->email }}</p>
+                                </div>
                             </div>
                         @endforeach
                     </div>
