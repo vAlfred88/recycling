ef8db878c5b77afee2b679b1121b260a37933820
vAlfred88 <valfred88@gmail.com>
2019-01-18 16:00:36 +0300

    Fix envoy deployer

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,33 @@
 @servers(['web' => 'root@37.228.118.147'])
 
-@task('deploy')
+@story('deploy-with-migration')
+git
+composer
+migrate
+seed
+@endstory
+
+@story('deploy')
+git
+composer
+@endstory
+
+@task('git')
 cd /var/www/html/vtorservice-laravel/
 git pull origin master
+@endtask
+
+@task('composer')
+cd /var/www/html/vtorservice-laravel/
+composer update
+@endtask
+
+@task('migrate')
+cd /var/www/html/vtorservice-laravel/
+php artisan migrate:fresh
+@endtask
+
+@task('seed')
+cd /var/www/html/vtorservice-laravel/
+php artisan db:seed
 @endtask
