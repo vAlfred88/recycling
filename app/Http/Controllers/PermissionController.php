b3a07d4c54c7f88f4606a610018d0b5069e1af02
vAlfred88 <valfred88@gmail.com>
2019-01-18 17:00:38 +0300

    Fix errors

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Permission;
-use App\Role;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class PermissionController extends Controller
 {
