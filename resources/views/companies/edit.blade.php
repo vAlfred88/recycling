a4e941d1b86e701e5f8faaa88a21a5a9149b4ef4
vAlfred88 <valfred88@gmail.com>
2019-01-31 15:36:11 +0300

    #12 Callback при возврате на карточку компании. (owner)

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -5,11 +5,19 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="white-box">
-                    <h3 class="box-title pull-left">{{ __('company.edit') }} {{ $company->name }}</h3>
-                    @can('update', $company)
-                        <a class="btn btn-success pull-right" href="{{ route('companies.index') }}">
-                            <i class="icon-arrow-left-circle" aria-hidden="true"></i> {{ __('pages.back') }}</a>
-                    @endcan
+                    <h3 class="box-title pull-left">Редактирование компании {{ $company->name }}</h3>
+                    @role('admin')
+                        @can('show-companies')
+                            <a class="btn btn-success pull-right" href="{{ route('companies.index') }}">
+                                <i class="icon-arrow-left-circle" aria-hidden="true"></i> {{ __('pages.back') }}</a>
+                        @endcan
+                    @endrole
+                    @hasanyrole('owner|employee')
+                        @can('show-companies')
+                            <a class="btn btn-success pull-right" href="{{ route('company') }}">
+                                <i class="icon-arrow-left-circle" aria-hidden="true"></i> {{ __('pages.back') }}</a>
+                        @endcan
+                    @endhasanyrole
                     <div class="clearfix"></div>
                     <hr>
 
