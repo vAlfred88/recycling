bd77342e4e8e7cecc56e33930243b6536dc5b21d
vAlfred88 <valfred88@gmail.com>
2019-02-13 18:33:39 +0300

    Исправлена ошибка роута в сайдбаре

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -6,7 +6,7 @@
                 <div class="profile-image">
                     <img src="{{ asset('images/metal.png') }}" alt="user-img" class="img-circle">
                     <p class="profile-text m-t-15 font-16">
-                        <a href="{{ route('companies.index') }}">{{ auth()->user()->company->name }}</a>
+                        <a href="{{ route('company') }}">{{ auth()->user()->company->name }}</a>
                     </p>
                 </div>
             </div>
