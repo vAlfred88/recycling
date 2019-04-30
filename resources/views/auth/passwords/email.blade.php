78f847cfd1969e34e9f6b68b395a6108a804a595
Andrei Bushev <morgan9757@gmail.com>
2019-04-30 11:41:35 +0300

    issue #56

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,40 +1,28 @@
-@extends('layouts.app')
+@extends('front.layouts.auth')
 
 @section('content')
-    <section id="wrapper" class="login-register">
-        <div class="login-box">
-            <div class="white-box">
-                <form class="form-horizontal form-material" method="POST"  action="{{ route('password.email') }}">
-                    @csrf
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <h3>{{ __('Reset Password') }}</h3>
-                            <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
-                        </div>
-                    </div>
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <input id="email" placeholder="Email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+    <div class="tbc">
+        <div class="log-reg-block inb">
+            <span class="title db alLeft">Добро пожаловать во <span class="org-name">Вторсервис</span>
+            </span>
+            <span class="db mess">Для восстановления пароля введите свой Email и вам будут высланы инструкции.</span>
 
-                            @if ($errors->has('email'))
-                                <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="form-group text-center m-t-20">
-                        <div class="col-xs-12">
-                            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            <form method="post"
+                  action="{{ route('password.email') }}">
+                @csrf
+                <div class="input-box">
+                    <input id="email" placeholder="Email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                </div>
+
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+                <div class="warning-modal__btn-box clearfix">
+                    <button class="reg-btn reg-btn_bg shadow-element fleft" type="submit">Восстановить</button>
+                </div>
+            </form>
         </div>
-    </section>
+    </div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,60 +1,47 @@
-@extends('layouts.app')
+@extends('front.layouts.auth')
 
 @section('content')
 
-    <section id="wrapper" class="login-register">
-        <div class="login-box">
-            <div class="white-box">
-                <form class="form-horizontal form-material" action="{{ route('password.request') }}">
-                    {{csrf_field()}}
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <h3>{{ __('Reset Password') }}</h3>
-                        </div>
-                    </div>
-                    <div class="form-group ">
+    <div class="tbc">
+        <div class="log-reg-block inb">
+            <span class="title db alLeft">Добро пожаловать в <span class="org-name">Вторсервис</span>
+            </span>
+            <span class="db mess">Восстановить пароль</span>
 
-                        <div class="col-xs-12">
-                            <input placeholder="Email" id="email" type="email"
-                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-                                   value="{{ $email ?: old('email') }}" required autofocus>
+            <form method="POST" action="{{ route('password.request') }}">
+                @csrf
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="input-box">
+                    <input placeholder="Email" id="email" type="email"
+                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                           value="{{ $email ?: old('email') }}" required autofocus>
 
-                            @if ($errors->has('email'))
-                                <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-xs-12">
-                            <input id="password" placeholder="Password" type="password"
-                                   class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                   name="password" required>
+                    <input id="password" placeholder="Password" type="password"
+                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                           name="password" required>
 
-                            @if ($errors->has('password'))
-                                <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
 
-                    <div class="form-group">
-                        <div class="col-xs-12">
-                            <input placeholder="Confirm Password" id="password-confirm" type="password" class="form-control"
-                                   name="password_confirmation" required>
-                        </div>
-                    </div>
-                    <div class="form-group text-center m-t-20">
-                        <div class="col-xs-12">
-                            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light"
-                                    type="submit">Reset
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                    <input placeholder="Confirm Password" id="password-confirm" type="password" class="form-control"
+                           name="password_confirmation" required>
+                </div>
+
+                <div class="warning-modal__btn-box clearfix">
+                    <button class="reg-btn reg-btn_bg shadow-element fleft" type="submit">Восстановить</button>
+                </div>
+            </form>
         </div>
-    </section>
+    </div>
 @endsection
