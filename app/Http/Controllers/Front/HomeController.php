d12e2ef5a9badbcc7c18e4ac588d286492567ea3
Andrei Bushev <morgan9757@gmail.com>
2019-05-03 14:56:48 +0300

    select to front

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers\Front;
 
 use App\Company;
 use App\Http\Controllers\Controller;
+use App\MetalCost;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $records_yesterday = MetalCost::whereDate('created_at',Carbon::yesterday())->get();
+        $records_today = MetalCost::whereDate('created_at',Carbon::today())->get();
         $companies = Company::query()
                             ->take(10)
                             ->orderBy('receptions_count', 'desk')
                             ->get();
 
-        return view('front.home', compact('companies'));
+        return view('front.home', compact('companies','records_today','records_yesterday'));
     }
 }
