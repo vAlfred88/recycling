96f2479106ca3e0b7371c12e52eced6e2c08351c
Andrei Bushev <morgan9757@gmail.com>
2019-05-14 09:59:02 +0300

    issue #73

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,13 +52,12 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-
         $validate = [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ];
-        if(!empty($data['company']))
+        if(!empty($data['company_field']))
         {
             $validate = array_add($validate,'company' , ['required', 'string', 'min:1','unique:companies,name']);
         }
