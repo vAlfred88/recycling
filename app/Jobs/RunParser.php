1400c59ccb2a8b8b1a424292ba7d7f62a029878d
Andrei Bushev <morgan9757@gmail.com>
2019-06-28 15:46:45 +0300

    parser fixes

---
+++ app/Jobs/RunParser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Classes\LmeParseService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class RunParser implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(LmeParseService $parser)
+    {
+        $result = false;
+        while (!$result) {
+            $result = $parser->fillDb();
+        }
+
+    }
+}
